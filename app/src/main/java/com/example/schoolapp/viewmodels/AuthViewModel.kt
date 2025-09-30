51fed69e6fe29f9893e2eaa983285bb7938a063e
ErickKaris <erickithandi@gmail.com>
2025-09-30 14:40:19 +0300

    Create a 'view model' class that defines the methods that contain logic to register user

---
+++ app/src/main/java/com/example/schoolapp/viewmodels/AuthViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.schoolapp.viewmodels
+
+import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.example.schoolapp.models.UserModel
+import com.example.schoolapp.navigation.ROUTE_LOGIN
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+class AuthViewModel: ViewModel() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun signup(username: String,
+               email: String,
+               password: String,
+               confirmPassword: String,
+               navController: NavController,
+               context: Context){
+        if (username.isBlank() || email.isBlank() || password.isBlank() || confirmPassword.isBlank()){
+            Toast.makeText(context,"Please fill all the fields", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (password != confirmPassword){
+            Toast.makeText(context, "Passwords do not match", Toast.LENGTH_LONG).show()
+        }
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+            task ->
+            if (task.isSuccessful){
+                val userId = auth.currentUser?.uid ?: ""
+                val user = UserModel(username=username, email=email,userId = userId)
+
+                saveUserToDatabase(user, navController, context)
+            }else{
+                Toast.makeText(context,"Registration failed", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    private fun saveUserToDatabase(user: UserModel, navController: NavController, context: Context){
+        val dbRef = FirebaseDatabase.getInstance().getReference("User/${user.userId}")
+        dbRef.setValue(user).addOnCompleteListener {
+            task ->
+            if (task.isSuccessful){
+                Toast.makeText(context, "User registered successfully", Toast.LENGTH_LONG).show()
+                navController.navigate(ROUTE_LOGIN){
+                    popUpTo(0)
+                }
+            }else{
+                Toast.makeText(context,"Failed to save user", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun login(){
+
+    }
+}
