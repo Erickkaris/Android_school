f4e851f27585576154c58d5487e3871b105671d9
ErickKaris <erickithandi@gmail.com>
2025-10-22 15:22:21 +0300

    Add new routes for splash screen and update/modify employee data screen

--- app/src/main/java/com/example/schoolapp/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/schoolapp/navigation/AppNavHost.kt
@@ -3,22 +3,37 @@ package com.example.schoolapp.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.schoolapp.ui.theme.screens.SplashScreen
 import com.example.schoolapp.ui.theme.screens.dashboard.dashboardScreen
 import com.example.schoolapp.ui.theme.screens.employees.EmployeeListScreen
 import com.example.schoolapp.ui.theme.screens.employees.addEmployeeScreen
+import com.example.schoolapp.ui.theme.screens.employees.updateEmployeeScreen
 import com.example.schoolapp.ui.theme.screens.login.loginScreen
 import com.example.schoolapp.ui.theme.screens.register.registerScreen
 
 @Composable
-fun AppNavHost(navController: NavHostController = rememberNavController(), startDestination: String = ROUTE_VIEWEMPLOYEE){
+fun AppNavHost(navController: NavHostController = rememberNavController(), startDestination: String = ROUTE_SPLASHSCREEN){
     NavHost(navController = navController, startDestination = startDestination){
-        composable (ROUTE_REGISTER){ registerScreen(navController) }
-        composable (ROUTE_LOGIN){ loginScreen(navController) }
-        composable (ROUTE_DASHBOARD){ dashboardScreen(navController) }
-        composable (ROUTE_ADDEMPLOYEE){ addEmployeeScreen(navController) }
-        composable (ROUTE_VIEWEMPLOYEE){ EmployeeListScreen(navController) }
+        composable (route = ROUTE_REGISTER){ registerScreen(navController) }
+        composable (route = ROUTE_LOGIN){ loginScreen(navController) }
+        composable (route = ROUTE_DASHBOARD){ dashboardScreen(navController) }
+        composable (route = ROUTE_ADD_EMPLOYEE){ addEmployeeScreen(navController) }
+        composable (route = ROUTE_VIEW_EMPLOYEE){ EmployeeListScreen(navController) }
+        composable (route = ROUTE_SPLASHSCREEN){ SplashScreen { navController.navigate(ROUTE_LOGIN)
+        {popUpTo(ROUTE_SPLASHSCREEN) { inclusive=true } } } }
+        composable(route = ROUTE_UPDATE_EMPLOYEE,
+            arguments = listOf(
+                navArgument("employeeId")
+                {type = NavType.StringType}
+            )){
+            backStackEntry ->
+            val employeeId = backStackEntry.arguments?.getString("employeeId")!!
+            updateEmployeeScreen(navController, employeeId)
+        }
     }
 }
--- app/src/main/java/com/example/schoolapp/navigation/Routes.kt
+++ app/src/main/java/com/example/schoolapp/navigation/Routes.kt
@@ -6,6 +6,10 @@ const val ROUTE_LOGIN = "login"
 
 const val ROUTE_DASHBOARD = "dashboard"
 
-const val ROUTE_ADDEMPLOYEE = "addEmployee"
+const val ROUTE_ADD_EMPLOYEE = "addEmployee"
 
-const val ROUTE_VIEWEMPLOYEE = "viewEmployee"
+const val ROUTE_VIEW_EMPLOYEE = "viewEmployee"
+
+const val ROUTE_UPDATE_EMPLOYEE = "update_employee/{employeeId}"
+
+const val ROUTE_SPLASHSCREEN = "splashScreen"
